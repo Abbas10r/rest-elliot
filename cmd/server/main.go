3c6bee496a176a9e0127de833b92905a2a9189fd
Abbas <abbastktL@gmail.com>
2025-03-06 01:46:29 +0600

    Added db connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"rest-elliot/internal/db"
+)
 
 // This func will be responsible for instaniation anmd startup of our app
 func Run() error {
 	fmt.Println("Starting out app")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to db")
+		return err
+	}
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+	fmt.Println("Succesfully connected and pinged db")
 	return nil
 }
 
